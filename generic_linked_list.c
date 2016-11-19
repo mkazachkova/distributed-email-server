d27af64e02cb8a31eb94e5a689bee562e0968cbe
Sarah Sukardi <sukardi@jhu.edu>
2016-11-19 15:14:51 -0500

    added empty methods

--- generic_linked_list.c
+++ generic_linked_list.c
@@ -7,6 +7,34 @@ December 9, 2016
 
 #include "generic_linked_list.h"
 
-int main() {
-  return 0;
+void create_list(List *list, size_t data_size, void (*fptr)(void *)) {
+  return;
+}
+void add_to_end(List *list, void *data) {
+  return;
+}
+
+void remove_from_beginning(List *list) {
+  return;
+}
+
+void remove_from_end(List *list) {
+  return;
+}
+
+void insert(List *list, void *data, void (*fptr)(void *)) {
+  return;
+}
+
+Node* find(List *list, void* data, void (*fptr)(void *)) {
+  return NULL;
+}
+
+// Accessor methods
+void print_list(List *list, void (*fptr)(void *)) {
+  return;
+}
+
+void print_list_backwards(List *list, void (*fptr)(void *)) {
+  return;
 }
