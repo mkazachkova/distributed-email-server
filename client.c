0f48bbaf80bc5eab68ba75e0957734cbc7b219b5
Sarah Sukardi <ssukard1@ugradx.cs.jhu.edu>
2016-11-30 15:41:33 -0500

    now multicasting in the way that publci servers will be communicated with

--- client.c
+++ client.c
@@ -34,8 +34,8 @@ static void Bye();
 //Variables that I (Sarah) added
 static int  curr_server = -1;
 static char curr_user[MAX_NAME_LEN];
-static bool connected_to_server = false;
 static char hardcoded_server_names[NUM_SERVERS][MAX_NAME_LEN];
+
 
 int main(int argc, char *argv[]) {
   int     ret;
@@ -145,8 +145,11 @@ static void User_command() {
         break;
       }
 
-      int mess_len = strlen(group);
-      SP_multicast(Mbox, AGREED_MESS, group, 2, mess_len, group);
+      int mess_len = strlen(hardcoded_server_names[curr_server]);
+      printf("Message of length %d with contents %s\n", mess_len, hardcoded_server_names[curr_server]);
+      
+      SP_multicast(Mbox, AGREED_MESS, hardcoded_server_names[curr_server], 2, mess_len, hardcoded_server_names[curr_server]);
+      
       /*
       ret = SP_join(Mbox, hardcoded_server_names[server_to_be_used]);
       if (ret < 0) SP_error(ret); 
