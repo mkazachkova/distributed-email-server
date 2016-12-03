7c3daabe3fe41f899bf8cd0169847366ac23e1a2
Sarah Sukardi <ssukard1@ugradx.cs.jhu.edu>
2016-12-02 21:31:23 -0500

    removed superfluous code

--- server.c
+++ server.c
@@ -67,7 +67,7 @@ December 9, 2016
 
 
 //CASE: client wants to display all headers
-//find specific use in linked list of users
+//find specific user in linked list of users
 //iterate through email linked list within the user and display (backwards) all emails with non-null data
 
 
@@ -93,6 +93,7 @@ December 9, 2016
 //use spread to print all servers currently in the partition
 
 
+
 /**********WHEN MEMBERSHIP CHANGES**********/
 //use anti entropy. boom. done
 
@@ -109,6 +110,7 @@ December 9, 2016
 //currently as possible sources of information (this is the vertical side of the array). You look at each column and find the max value (only
 //considering the processes that are in the partition). The process with the max value is responsible for sending the updates to all other processes
 //in the partition
+
 
 #define int32u unsigned int
 
@@ -218,7 +220,7 @@ int main(int argc, char *argv[]) {
 }
 
 
-
+ 
 static void Respond_To_Message() {
   
   //static  char  mess[MAX_MESSLEN];
@@ -230,29 +232,6 @@ static void Respond_To_Message() {
   int   endian_mismatch;
 
 
-  /*
-  int ret = SP_receive(Mbox, &service_type, sender, 100, &num_groups, target_groups,
-             &mess_type, &endian_mismatch, sizeof(mess), mess);
-
-  printf("this is ret: %d\n", ret);
-  printf("%d\n", service_type);
-
-
-  if ( Is_caused_join_mess(service_type)) {
-    printf("we have received a membership message\n");
-  }
-
-  
-  ret = SP_receive(Mbox, &service_type, sender, 100, &num_groups, target_groups,
-                       &mess_type, &endian_mismatch, sizeof(mess), mess);
-
-  printf("this is ret: %d\n", ret);
-  printf("%d\n", service_type);
-
-  if  (Is_caused_join_mess(service_type)) {
-    printf("we have received a membership message\n");
-  }
-  */
   //  InfoForServer *info = malloc(sizeof(InfoForServer));
   //ret = SP_receive(Mbox, &service_type, sender, 100, &num_groups, target_groups,
   //               &mess_type, &endian_mismatch, sizeof(InfoForServer), (char*)info);
@@ -491,14 +470,6 @@ void print_email(void *email) {
 }
 
 /*
-static void Print_menu();
-static void User_command();
-static void Read_message();
-static void Usage( int argc, char *argv[] );
-static void Print_help();
-static void Bye();
-*/
-/*
 int main(int argc, char *argv[]) {
   int     ret;
   int     mver, miver, pver;
@@ -594,9 +565,6 @@ static void Read_message() {
 
     //TODO: process messages received
 
-
-
-
   //Change of membership detected
   } else if (Is_membership_mess(service_type)) {
     ret = SP_get_memb_info( mess, service_type, &memb_info );
@@ -676,63 +644,8 @@ static void Bye() {
 
 /*
 
-
 int compareEmail(void* temp1, void* temp2);
 void printEmail(void *n);
-
-int main() {
-  List list;
-
-  //Create a linked list
-  create_list(&list, sizeof(Email));
-
-  Email *temp1 = malloc(sizeof(Email));
-  temp1->emailInfo.timestamp.counter = 1;
-  temp1->emailInfo.timestamp.machine_index = 0;
-  temp1->emailInfo.timestamp.message_index = 1;
-
-  add_to_end(&list, temp1);
-
-  print_list(&list, printEmail);
-
-
-  Email *temp2 = malloc(sizeof(Email));
-  temp2->emailInfo.timestamp.counter = 2;
-  temp2->emailInfo.timestamp.machine_index = 0;
-  temp2->emailInfo.timestamp.message_index = 2;
-
-  insert(&list, temp2, compareEmail);
-  print_list(&list, printEmail);
-
-
-
-  Email *temp3 = malloc(sizeof(Email));
-  temp3->emailInfo.timestamp.counter = 4;
-  temp3->emailInfo.timestamp.machine_index = 2;
-  temp3->emailInfo.timestamp.message_index = 3;
-
-
-  insert(&list, temp3, compareEmail);
-  print_list(&list, printEmail);
-  
-  Email *temp4 = malloc(sizeof(Email));
-  temp4->emailInfo.timestamp.counter = 3;
-  temp4->emailInfo.timestamp.machine_index = 2;
-  temp4->emailInfo.timestamp.message_index = 4;
-
-  insert(&list, temp4, compareEmail);
-  print_list(&list, printEmail);
-
-  Email *temp5 = malloc(sizeof(Email));
-  temp5->emailInfo.timestamp.counter = 3;
-  temp5->emailInfo.timestamp.machine_index = 0;
-  temp5->emailInfo.timestamp.message_index = 100;
-
-  insert(&list, temp5, compareEmail);
-  print_list(&list, printEmail);
-  
-}
-
 
 
 int compareEmail(void* temp1, void* temp2) {
