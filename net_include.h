b07fff358088c59ec4390ede7403f9cf2bf0f5a8
Sarah Sukardi <ssukard1@ugradx.cs.jhu.edu>
2016-12-05 15:29:04 -0500

    modified struct for info for client

--- net_include.h
+++ net_include.h
@@ -124,9 +124,11 @@ typedef struct info_for_client {
   int    type;                                       // 1 is list headers
                                                      // 2 is specific email body to send
                                                      // 3 is print membership
-
+                                                     // 4 is "client would like to join" message
+  
   Header headers[MAX_HEADERS_IN_PACKET];             // used for sending headers
   Email  email;                                      // used for sending specific email body
   char   memb_identities[NUM_SERVERS][MAX_NAME_LEN]; // used for sending membership identities
+  char   client_server_group_name[MAX_NAME_LEN]      // used for sending the group name for "client would like to join" message
 } InfoForClient;
 
