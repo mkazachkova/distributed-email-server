567b9ae12c23b0cbfaa11d9e28f68a0a769d8a5c
Sarah Sukardi <sukardi@jhu.edu>
2016-12-04 20:35:46 -0500

    added comments and fixed minimum index retrieval bug (before it would always have just iterated from 0

--- server.c
+++ server.c
@@ -8,108 +8,6 @@ December 9, 2016
 #include "sp.h"
 #include "net_include.h"
 
-/**********RECEIVING************/
-//CASE: received type 4 update (new user has been created)
-//take name off of the update
-//iterate through your user array and see if that user exists
-//if not, create a new user object with the username from the update
-//append update to the corresponding index to the machine that sent it (append to linked list)
-//(do something with updating the counter)
-
-
-//CASE: received type 1 update (new email)
-//first, check if that email already exists. If it does, just store the update
-//if email doesn't exist, take to field off of email and find that user in your user linked list
-//if user doesn't exist, create new user using the to field (****double check this, might just display an error)
-//if user to does exist, find that object is user linked list and access the email linked list of that user
-//using the timestamps insert the email into the correct location for that email
-//append update to the corresponding index to the machine that sent it (append to linked list)
-//(do something with counter maybe)
-
-
-//CASE: receive type 2 update (read)
-//first, check if user whose mailbox you'll be modifiying actually exists or not
-//if user doesn't exist, create new user object with name of user. Then, create a new node in the linked list of emails for that user
-//with null data. Set the read bool equal to true for that specific email
-//If user does exist, loop through linked list of email to find the email with that specific time stamp
-//if email doesn't exist then create a new node and insert it into the linked list where the email should have been.
-//set the data for this node to null, and set the bool for read to true.
-//If email does exist, set the the read bool to true. If already true, then can set it to true again without anything going wrong
-
-
-//CASE: receive type 3 update (delete)
-//first, check if user whose mailbox you'll be modifiying actually exists or not
-//if user doesn't exist, create new user object with name of user. Then, create a new node in the linked list of emails for that user
-//with null data. Set the delete bool equal to true for that specific email
-//If user does exist, loop through linked list of email to find the email with that specific time stamp
-//if email doesn't exist then create a new node and insert it into the linked list where the email should have been.
-//set the data for this node to null, and set the bool for delete to true.
-//If email does exist, set the the delete bool to true. If already true, then can set it to true again without anything going wrong
-
-
-
-/**********CLIENT FUNCTIONS**********/
-//CASE: client login to server (new client)
-//Client and server are put into private group (membership and spread stuff)
-//Server x (server that user is on) creates a new user object and appends it to user linked list
-//Server x sends all other servers (multicast) in its partition an update saying a new user has joined
-
-
-//CASE: client login to server (returning client)
-//Client and server in private group
-//Server x iterates through user linked list and finds user object. No update is sent and no new user object is created
-
-
-//CASE: client switches servers
-//private group between client and original server is dropped
-//private group between client and new server is created
-
-
-//CASE: client wants to display all headers
-//find specific user in linked list of users
-//iterate through email linked list within the user and display (backwards) all emails with non-null data
-
-
-//CASE: client sends an email
-//client is prompted for to, subject, and message.
-//Server x (the server the user is currently on) multicasts an update with type 1 signifiying a new email (first create email
-//and update object; attach timestamp as needed)
-
-//CASE: client wants to delete a message
-//client selects email to delete after displaying the headers (deleting yth email)
-//iterate from tail to find yth email; take time stamp off of this email and store as t. Change data in email to null
-//note for above: need to iterate over non-null data
-//server x (the server the user is currently on) sends an update (type 3) with a timestamp_of_email (this will be t) and its
-//own timestamp as well 
-
-
-//CASE: client reads an email
-//client selects an email to read. The message of the email is diplayed to the client (iterate through linked list from tail
-//and that the xth email ignoring null data). Server x sends an update (type 2) with the timestamp of the email that was read
-//along with its own timestamp and multicasts it out
-
-//CASE: client wants to print memberships
-//use spread to print all servers currently in the partition
-
-
-
-/**********WHEN MEMBERSHIP CHANGES**********/
-//use anti entropy. boom. done
-
-//we attach a 1d array with what we (as a server) know from every other server to every update that we send. When a server receives this
-//they add it to their 2d array of what all servers know from all other servers.
-
-//When there is a merge each server involved in the merge multicasts out a struct (merge_matrix) containing its own 2d matrix.
-
-//When you receive one of these merge_matrix structs you iterate through it and compare each cell to the corresponding cell
-//in your own 2d array. You take the max of the values and stre that as the cell in your 2d array. You must wait until you receive however many
-//servers there are in the new partition worth of these merge_matrix structs. Ignore the merge_matrix struct you receive from yourself.
-
-//once your 2d array is loaded with all of the new information, you must only consider the servers that you are in a partition with
-//currently as possible sources of information (this is the vertical side of the array). You look at each column and find the max value (only
-//considering the processes that are in the partition). The process with the max value is responsible for sending the updates to all other processes
-//in the partition
-
 
 #define int32u unsigned int
 
@@ -120,7 +18,6 @@ static char         Spread_name[80];
 static char         Private_group[MAX_GROUP_NAME];
 static mailbox      Mbox;
 //static int          Num_sent;
-//static unsigned int Previous_len;
 
 static char         group[80] = "ssukard1mkazach1_2"; 
 char                server_own_group[80] = "ssukard1mkazach1_server_";
@@ -144,6 +41,8 @@ int                 lamport_counter = 0;
 int                 num_emails_checked = 0;
 int                 min_seen_global = -1;
 
+
+// Global variables used for sending header to client
 int                 message_number_stamp = 1;
 int                 num_headers_added;
 InfoForClient       *client_header_response;     
@@ -168,10 +67,11 @@ static void   add_to_header(Email *email);
 static int    max(int one, int two);
 static int    min(int one, int two);
 
+
 // **************************** MAIN METHOD **************************** //
 int main(int argc, char *argv[]) {
 
-  //error check input
+  //Error check input
   if (argc <= 1) {
     printf("Please input the server number (1-5) as a command-line input.\n");
     exit(0);
@@ -261,7 +161,6 @@ static void Respond_To_Message() {
   //maybe we create a separate method for creating a user when we get an email, read, or
   //delete for a user that has not been created
   
-
   
   // Putting whatever was received into tmp_buf, which will be cast into whatever type
   // it is by looking at the first digit!
@@ -271,15 +170,17 @@ static void Respond_To_Message() {
 
   printf("THIS IS SERVICE TYPE: ! %d\n", service_type);
 
+
+  // **************************** PARSE MEMBERSHIP MESSAGES **************************** //
+
+  //If transition message, do nothing
   if (Is_transition_mess(service_type)) {
+    printf("Transition message received!\n");
     return;
   }
-  // **************************** PARSE MEMBERSHIP MESSAGES **************************** //
 
   //Parsing the server entering message (Should only be executed at the beginning of the program)
   if (Is_caused_join_mess(service_type)) {
-    //int num = atoi(&(target_groups[0][strlen(target_groups[0]) - 1]));
-    //printf("num in caused by join: %d\n", num);
 
     printf("A join message was received. Here are the contents of target_groups...\n");
     for (int i = 0; i < num_groups; i++) {
@@ -320,11 +221,15 @@ static void Respond_To_Message() {
     return;
   }
 
-  //TODO: Will be fleshed out when we work on actual network partitions
+
+  //This method is triggered whenever there is a change in membership detected!
+  //TODO: debug with actual network partitions
   if (Is_caused_network_mess(service_type)) {
-    printf("change in membership has occured!\n");
+    printf("A change in membership has occurred!\n");
+
+    //Set all servers in partition to false
     for (int i = 0; i < NUM_SERVERS; i++) {
-      servers_in_partition[i] = 0;
+      servers_in_partition[i] = false;
     }
     num_servers_in_partition = 0;
 
@@ -334,93 +239,131 @@ static void Respond_To_Message() {
       servers_in_partition[num] = true;
       num_servers_in_partition++;
     }
+
+    //Print all values in target_group (all the members in our partition) -- for debug
     for (int i = 0; i < num_groups; i++) {
       printf("%s\n", target_groups[i]);
     }
 
-    //call function to do merge stuff
+    //Allocate space for and populate MergeMatrix sent to other servers
     MergeMatrix *merge = malloc(sizeof(MergeMatrix));
     merge->type = 20;
     merge->machine_index = my_machine_index;
+
+    //Copy local merge matrix into MergeMatrix to be sent
     for (int i = 0; i < NUM_SERVERS; i++) {
       memcpy(merge->matrix[i], merge_matrix[i], sizeof(merge_matrix[i]));
     }
+
+    //Print merge matrix sent (for debug)
     for (int i = 0; i < NUM_SERVERS; i++) {
       for (int j = 0; j < NUM_SERVERS; j++) {
         printf("%d ", merge->matrix[i][j]);
       }
       printf("\n");
     }
-    //send 3d array to everyone else
-    SP_multicast(Mbox, AGREED_MESS, group, 2, sizeof(MergeMatrix), (char*)merge);
 
+    //send 2d MergeMatrix array to everyone else
+    SP_multicast(Mbox, AGREED_MESS, group, 2, sizeof(MergeMatrix), (char*)merge);
+    
+    //Free to prevent memory leaks
     free(merge);
     merge = NULL;
-    //receive num_groups number of 2d arrays
+
+    //receive num_groups number of 2D arrays
     int num_matrices_received = 0;
     int min_array[NUM_SERVERS][NUM_SERVERS];
 
+    //fill min_array with 0 initially
     for (int i = 0; i < NUM_SERVERS; i++) {
       for (int j = 0; j < NUM_SERVERS; j++) {
         min_array[i][j] = 0;
       }
     }
     
+    //Iterate until all (num_groups number of) merge matrices have been processed
     for (;;) {
+      //Allocate space for a MergeMatrix and receive it from sender
       MergeMatrix *merge = malloc(sizeof(MergeMatrix));
       SP_receive(Mbox, &service_type, sender, 100, &num_groups, target_groups,
                  &mess_type, &endian_mismatch, sizeof(MergeMatrix), (char*)merge);
       num_matrices_received++;
-      //now we want to process it
+
+      //Process MergeMatrix
       for (int i = 0; i < NUM_SERVERS; i++) {
         for (int j = 0; j < NUM_SERVERS; j++) {
+          // populate the min_array (min seen in all merge matrices sent)
           min_array[i][j] = min(merge_matrix[i][j], merge->matrix[i][j]);
+          // update the local merge_matrix (max seen in all merge matrices sent)
           merge_matrix[i][j] = max(merge_matrix[i][j], merge->matrix[i][j]);
         }
       }
+
+      //Exit loop once num_groups merge matrices have been received
       if (num_matrices_received == num_groups) {
         break;
       }
-    }  
-    //now both arrays loaded; figure out which machine needs to send
-    int who_sends[NUM_SERVERS] = { -1 };
+    }
+
+
+    //now both (current merge_matrix and min_array) arrays loaded
+    //it's time to figure out which machine needs to send updates
+    int who_sends[NUM_SERVERS] = { -1 }; //array of who must send updates
     int max_seen = 0;
     printf("This is servers in partition array: \n");
+    
+    //For debug: print servers in our new partition
     for (int i = 0; i < NUM_SERVERS; i++) {
       printf("%d ", servers_in_partition[i]);
     }
+
+    //Find which server has the maximum
     for (int i = 0; i < NUM_SERVERS; i++) {
       for (int j = 0; j < NUM_SERVERS; j++) {
         if (servers_in_partition[j]) {
-          if (merge_matrix[j][i] > max_seen) {
+          //If a greater value seen, set the value of max_seen, as well as
+          //update who must send values in that column then
+          if (merge_matrix[j][i] > max_seen) {            
             max_seen = merge_matrix[j][i];
             who_sends[i] = j;
           }
         }
       }
+      //reset max_seen back to 0
       max_seen = 0;
     }
+
+    //For debug: Print the who sends array to see who is sending what updates
     printf("printing who sends array!\n");
     for (int i = 0; i < NUM_SERVERS; i++) {
       //assert(who_sends[i] >= 0);
       printf("%d ", who_sends[i]);
     }
 
+    //Do the actual sending of messages
     int min_seen = INT_MAX;
     for (int i = 0; i < NUM_SERVERS; i++) {
-      min_seen = 0;
-      if (who_sends[i] == my_machine_index) {
-        //this means we must send; we must find the min for the process we are sending for and send from there until the end
+      min_seen = INT_MAX;
+
+      //If who_sends[i] is our machine index, WE must send      
+      if (who_sends[i] == my_machine_index) {        
+        //Find the min for the process we are sending for and send from there until the end
         for (int j = 0; j < NUM_SERVERS; j++) {
           if (min_array[j][i] < min_seen) {
             min_seen = min_array[j][i];
           }
         }
+
+        //Set the min_seen_global variable to the minimum we have seen 
+        //(global only set here for safety)
         min_seen_global = min_seen;
-        //now we actually need to send!
+
+        // now we actually do the sending
         forward_iterator(&(array_of_updates_list[i]), send_updates_for_merge);
       }
+
     }
+
     return;
   }
 
@@ -433,7 +376,6 @@ static void Respond_To_Message() {
 
 
   // ****************************** parse messages from client ***************************** //
-
 
   //If *type is of type 2-7, we have RECEIVED A MESSAGE FROM THE CLIENT.
 
