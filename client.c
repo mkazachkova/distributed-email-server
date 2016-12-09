8f54ec8d3683d9cb17cc6a9e7fc8abb01d859720
Sarah Sukardi <sukardi@jhu.edu>
2016-12-09 15:14:42 -0500

    claned up client code

--- client.c
+++ client.c
@@ -145,18 +145,12 @@ static void User_command() {
   ///////////////////////////////////// LOGIN WITH A USERNAME ///////////////////////////////////
   case 'u': {
     ret = sscanf( &command[2], "%s", curr_user);
-    //printf("this is curr user: %s\n", curr_user);
 
-    // char temp_buff[50] = "";
-    //sscanf( &command[strlen(curr_user) + 2], "%s", temp_buff);
-    //    printf("this is temp_buff: %s\n", temp_buff);
-    //printf("this is the third command thing: %d\n", command[3]);
-
-    //strcat(curr_user, temp_buff);
     printf("This is curr user: %s\n", curr_user);
     empty_list(&headers_list);
     
-    if (logged_in && connected_to_server) { /*DOUBLE CHECK THIS. MY THINKING IS WE SHOULDNT SEND ANYTHING TO SERVER IF NOT CONNECTED*/
+    /*DOUBLE CHECK THIS. MY THINKING IS WE SHOULDNT SEND ANYTHING TO SERVER IF NOT CONNECTED*/
+    if (logged_in && connected_to_server) {       
       //alert the server that a (potentially) new user might need to be created
       InfoForServer *new_user_request = malloc(sizeof(InfoForServer));
       new_user_request->type = 1; //for new user
@@ -204,7 +198,8 @@ static void User_command() {
     sprintf(connect_server_request->user_name, "%s",curr_user); //to be changed when we implement getting the user name
 
     //Send initial request to log in to the server
-    int ret = SP_multicast(Mbox, AGREED_MESS, hardcoded_server_names[curr_server], 2, sizeof(InfoForServer), (char*)connect_server_request);    
+    int ret = SP_multicast(Mbox, AGREED_MESS, hardcoded_server_names[curr_server], 2, sizeof(InfoForServer), (char*)connect_server_request);
+
     if (ret < 0) {
       SP_error(ret);
       printf("Error occurred in SP_multicast.\n");
@@ -250,10 +245,6 @@ static void User_command() {
           free(info);
           continue;
         }
-
-        printf("this is service type: %d\n", service_type);
-        printf("this is info type: %d\nThis is sender: %s\n", info->type, sender);
-
         
         if (info->type == 4) {
           //Now we connect to whatever name the server has sent back to us
@@ -351,9 +342,6 @@ static void User_command() {
         
       mess_len += strlen(&mess[mess_len]);
     }
-
-    //for debug
-    //printf("this is to: %s\nThis is subject: %s\nThis is message: %s\n", to, subject, mess);
 
     //Send the new message to the server
     InfoForServer *new_message_request = malloc(sizeof(InfoForServer));
@@ -479,7 +467,7 @@ static void User_command() {
   }
 
   case 's': {
-    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     break;
   }
       
@@ -503,19 +491,11 @@ static void User_command() {
 static void Read_message() {
 
   static  char              sender[MAX_GROUP_NAME];
-          //char              mess[MAX_MESSLEN];
           char              target_groups[MAX_MEMBERS][MAX_GROUP_NAME];
-          //membership_info   memb_info;
-          //vs_set_info       vssets[MAX_VSSETS];
-          //unsigned int      my_vsset_index;
-          //int               num_vs_sets;
-          //char              members[MAX_MEMBERS][MAX_GROUP_NAME];
           int               num_groups;
           int               service_type;
           int16             mess_type;
           int               endian_mismatch;
-          //int               i,j;
-          //int               ret;
 
           service_type = 0;
 
@@ -525,8 +505,6 @@ static void Read_message() {
   printf("\nATTN: MESSAGE RECEIVED W/ SERVICE TYPE: %d FROM %s SIZE %d\n", service_type, sender, ret);
 
   if (Is_transition_mess(service_type)) {
-    //printf("Transition message received.\n");
-
     free(tmp_buf);
     printf("User> ");
     fflush(stdout);
@@ -534,7 +512,6 @@ static void Read_message() {
   }
 
   if (Is_caused_network_mess(service_type)) {
-    //printf("Network message received- partition or merge occurred!\n");
     printf("**********You have been disconnected from your server. Please try connecting to another one**********.\n");
 
     //Make curr_group_connected_to back to empty string
@@ -542,7 +519,6 @@ static void Read_message() {
     strcpy(curr_group_connected_to, "");
 
     connected_to_server = false; //since we have been disconnected 
-
     
     free(tmp_buf);
     printf("User> ");
@@ -551,7 +527,6 @@ static void Read_message() {
   }
   
   if (Is_caused_join_mess(service_type)) {
-    //printf("Join message received!\n");
     free(tmp_buf);
     printf("User> ");
     fflush(stdout);
@@ -559,7 +534,6 @@ static void Read_message() {
   }
 
   if (Is_caused_leave_mess(service_type)) {
-    printf("Leave message received!\n");
     free(tmp_buf);
     printf("User> ");
     fflush(stdout);
@@ -578,7 +552,6 @@ static void Read_message() {
 
     bool done = info_header->done;
     
-    //can_print_user = false;
     for (int i = 0; i < MAX_HEADERS_IN_PACKET; i++) {
       if (info_header->headers[i].message_number != -1) {
         //print the header on the screen for the client
@@ -588,12 +561,6 @@ static void Read_message() {
         add_header(&(info_header->headers[i])); 
         printf("\n");
       }
-
-      //else {
-        //can_print_user = true;
-        //break;
-      //}
-      
     }
     
     //Receive the rest of the headers
@@ -610,7 +577,6 @@ static void Read_message() {
           add_header(&(info_header_t->headers[i])); 
           printf("\n");
         }
-        
       }
 
       done = info_header_t->done;
@@ -647,129 +613,23 @@ static void Read_message() {
     printf("SOMETHING UNDEFINED RECEIVED!!\n");
   }
 
-  
-
-  /*  
-  ret = SP_receive( Mbox, &service_type, sender, 100, &num_groups, target_groups, 
-    &mess_type, &endian_mismatch, sizeof(mess), mess );
-  printf("\n============================\n");
-  if (ret < 0) {
-    if ( (ret == GROUPS_TOO_SHORT) || (ret == BUFFER_TOO_SHORT) ) {
-      service_type = DROP_RECV;
-      printf("\n========Buffers or Groups too Short=======\n");
-      ret = SP_receive( Mbox, &service_type, sender, MAX_MEMBERS, &num_groups, target_groups, 
-                        &mess_type, &endian_mismatch, sizeof(mess), mess );
-    }
-  }
-  if (ret < 0) {
-    if (!To_exit) {
-      SP_error(ret);
-      printf("\n============================\n");
-      printf("\nBye.\n");
-    }
-    exit(0);
-  }
-
-  //Regular type of message received (non-membership)
-  if (Is_regular_mess(service_type)) {
-    mess[ret] = 0;
-    
-    //for debugging 
-    if      (Is_unreliable_mess(service_type)) printf("received UNRELIABLE\n");
-    else if (Is_reliable_mess(  service_type)) printf("received RELIABLE\n");
-    else if (Is_fifo_mess(      service_type)) printf("received FIFO\n");
-    else if (Is_causal_mess(    service_type)) printf("received CAUSAL\n");
-    else if (Is_agreed_mess(    service_type)) printf("received AGREED\n");
-    else if (Is_safe_mess(      service_type)) printf("received SAFE\n");
-
-    printf("message from %s, of type %d, (endian %d) to %d groups \n(%d bytes): %s\n",
-      sender, mess_type, endian_mismatch, num_groups, ret, mess );
-
-    //TODO: process messages received
-
-
-
-
-  //Change of membership detected
-  } else if (Is_membership_mess(service_type)) {
-    ret = SP_get_memb_info( mess, service_type, &memb_info );
-    if (ret < 0) {
-      printf("BUG: membership message does not have valid body\n");
-      SP_error( ret );
-      exit( 1 );
-    }
-
-    if (Is_reg_memb_mess(service_type)) {
-      printf("Received REGULAR membership for group %s with %d members, where I am member %d:\n",
-        sender, num_groups, mess_type );
-
-      for (i = 0; i < num_groups; i++) {
-        printf("\t%s\n", &target_groups[i][0]);
-      }
-
-      printf("grp id is %d %d %d\n",memb_info.gid.id[0], memb_info.gid.id[1], memb_info.gid.id[2] );
-
-      if (Is_caused_join_mess(service_type)) {
-        printf("Due to the JOIN of %s\n", memb_info.changed_member);
-      } else if (Is_caused_leave_mess( service_type)) {
-        printf("Due to the LEAVE of %s\n", memb_info.changed_member);
-      } else if (Is_caused_disconnect_mess( service_type)) {
-        printf("Due to the DISCONNECT of %s\n", memb_info.changed_member);
-      } else if (Is_caused_network_mess( service_type)) {
-        printf("Due to NETWORK change with %u VS sets\n", memb_info.num_vs_sets);
-        num_vs_sets = SP_get_vs_sets_info( mess, &vssets[0], MAX_VSSETS, &my_vsset_index);
-        if (num_vs_sets < 0) {
-          printf("BUG: membership message has more then %d vs sets. Recompile with larger MAX_VSSETS\n", MAX_VSSETS);
-          SP_error(num_vs_sets);
-          exit(1);
-        }
-
-
-        for (i = 0; i < num_vs_sets; i++) {
-          printf("%s VS set %d has %u members:\n",
-                 (i == my_vsset_index) ? "LOCAL" : "OTHER", i, vssets[i].num_members);
-          ret = SP_get_vs_set_members(mess, &vssets[i], members, MAX_MEMBERS);
-          if (ret < 0) {
-            printf("VS Set has more then %d members. Recompile with larger MAX_MEMBERS\n", MAX_MEMBERS);
-            SP_error(ret);
-            exit(1);
-          }
-          for (j = 0; j < vssets[i].num_members; j++) {
-            printf("\t%s\n", members[j] );
-          }
-        }
-      }
-
-    } else if (Is_transition_mess(service_type)) {
-      printf("received TRANSITIONAL membership for group %s\n", sender);
-    } else if (Is_caused_leave_mess(service_type)) {
-      printf("received membership message that left group %s\n", sender);
-    } else {
-      printf("received incorrect membership message of type 0x%x\n", service_type);
-    }
-  } else if (Is_reject_mess(service_type)) {
-    printf("REJECTED message from %s, of servicetype 0x%x messtype %d, (endian %d) to %d groups \n(%d bytes): %s\n",
-      sender, service_type, mess_type, endian_mismatch, num_groups, ret, mess );
-  } else {
-    printf("received message of unknown message type 0x%x with ret %d\n", service_type, ret);
-  }
-  */
   printf("\n");
   printf("User> ");
   fflush(stdout); 
 }
 
 static void print_header(Header *header) {
-  //printf("%d. %s\n", header->message_number, !(header->read) ? "***NEW MESSAGE!***" : "");
   printf("***************** MESSAGE NUMBER: %d *******************\n", header->message_number);
   printf("From: %s\nSubject: %s\nRead: %d\n", header->sender, header->subject, header->read);
   printf("--------------------------------------------------------\n");
 }
 
+
 //All headers should be added in consecutive order
 static void add_header(Header *header) {
   add_to_end(&headers_list, (void*) header);
 }
+
 
 static TimeStamp* get_timestamp_associated_with_header(int num) {
   Header* found_header = (Header *) find(&headers_list, &num, compare_header_num);
@@ -784,6 +644,7 @@ static int compare_header_num(void* h1, void* h2) {
 
   return (header_in_linked_list->message_number == *desired_header_num) ? 0 : 1;
 }
+
 
 //Takes in command-line args for spread name and user
 static void Usage(int argc, char *argv[]) {
@@ -809,6 +670,7 @@ static void Usage(int argc, char *argv[]) {
   }
 }
 
+
 //Print the help menu
 static void Print_help() {
   printf( "Usage: spuser\n%s\n%s\n%s\n",
@@ -817,6 +679,7 @@ static void Print_help() {
     "\t[-r ]             : use random user name");
   exit( 0 );
 }
+
 
 //Exit the program
 static void Bye() {
